3152d95575f2e541ac69b9844582ba552fd56767
Ali Alpay <alialpay96@hotmail.com>
2021-03-25 00:06:07 +0300

    Mezuniyet

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -34,7 +34,7 @@ namespace Business.Concrete
         //[LogAspect] --> AOP
         //Claim
         
-        //[SecuredOperation("product.add,admin")]
+        [SecuredOperation("product.add,admin")]
         [ValidationAspect(typeof(ProductValidator))]
         [CacheRemoveAspect("IProductService.Get")]
         public IResult Add(Product product)
